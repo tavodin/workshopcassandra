56d5fe1bac9ce95fc0729533ab9a5bca7ef9c44e
Gustavo Teixeira <gustavo.teixeira2199@gmail.com>
2024-10-30 22:07:11 -0300

    Initial setup

---
+++ src/main/java/com/tavodin/workshopcassandra/model/entities/Departments.java
@@ -0,0 +1,38 @@
+package com.tavodin.workshopcassandra.model.entities;
+
+import org.springframework.data.cassandra.core.mapping.PrimaryKey;
+import org.springframework.data.cassandra.core.mapping.Table;
+
+import java.util.UUID;
+
+@Table(value = "departments")
+public class Departments {
+
+    @PrimaryKey
+    private UUID id;
+    private String name;
+
+    public Departments() {
+    }
+
+    public Departments(UUID id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+    public UUID getId() {
+        return id;
+    }
+
+    public void setId(UUID id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
